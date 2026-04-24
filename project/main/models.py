030310e71c3aac8b400c518841f5d1f124a3cab0
Abduazim Abdumuxtorov <asadillomurotaliyef@gmail.com>
2026-04-24 20:30:23 +0500

    Update models.py

--- project/main/models.py
+++ project/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
-    country = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -11,10 +11,10 @@ class Brand(models.Model):
         return f"Pk: {self.id}, name: {self.name}"
 
 class Car(models.Model):
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='cars')
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     model_name = models.CharField(max_length=100)
     year = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10)
 
     def __str__(self):
         return f"{self.brand.name} {self.model_name}"
